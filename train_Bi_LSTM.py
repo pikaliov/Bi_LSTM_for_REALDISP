80b81b00679bc0469d1034c1aad63d9938e92f6a
Fese <fabian.fesenmayr@gmail.com>
2017-12-15 12:41:50 +0100

    save model on disk added cohens kappa as metric

--- train_Bi_LSTM.py
+++ train_Bi_LSTM.py
@@ -2,11 +2,13 @@ from matplotlib import pyplot
 from pandas import read_csv
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import f1_score
+from sklearn.metrics import cohen_kappa_score
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Bidirectional
 from keras.layers import Dropout
+from keras.models import load_model
 import numpy as np
 import custom_metrics as cus_met
 
@@ -16,15 +18,35 @@ def validate_dataset(name, model, n_timesteps, n_classes):
     dataset = read_csv('prepared_' + name + '.csv', header=0, index_col=0)
 
     # split the dataset into input and output data and reshape input for LSTM [samples, timesteps, features]
-    values_X, values_y = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes)
+    values_X, values_y, values_y_dec = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes)
     y_pred = model.predict(values_X)
 
     # make final prediction => transform float prediction values to int
     # (Softmax => max value in a row = highest likelihood)
     y_pred_final = (y_pred == y_pred.max(axis=1, keepdims=True)).astype(int)
-    f1_score_val = f1_score(values_y, y_pred_final, average='macro')
 
-    print('F1-Score for ' + name + ': %f' % (f1_score_val))
+    # transform the predicted binary matrix in a decimal vector
+    y_pred_dec = np.empty(y_pred_final.shape[0], dtype=np.int)
+    iterator = np.nditer(y_pred_final, flags=['multi_index'])
+    while not iterator.finished:
+        if (iterator[0] == 1):
+            mi = iterator.multi_index
+            y_pred_dec[mi[0]] = mi[1]
+        iterator.iternext()
+
+    # F1-Score
+    f1_score_val = f1_score(values_y, y_pred_final, average='micro')
+    print('\nF1-Score for ' + name + ': %f' % (f1_score_val))
+
+    # Cohen's Kappa Score (compare the decimal vectors)
+    kappa_score = cohen_kappa_score(values_y_dec, y_pred_dec)
+    print('Cohens-Kappa-Score for ' + name + ': %f' % (kappa_score))
+
+    # Accuracy Score
+    scores = model.evaluate(values_X, values_y, verbose=0)
+    print('Test accuracy for ' + name + ': ', scores[1])
+
+
 
 def split_dataset_into_input_and_output(dataset, timesteps, n_classes ):
 
@@ -55,20 +77,20 @@ def split_dataset_into_input_and_output(dataset, timesteps, n_classes ):
     # keep only the target y from the end of the sequence
     # (1 sequence of x-timesteps = 1 target y)
     # TODO: select the the target y which is dominated in the last column
-    val_X, val_y = val[:, :, :-1], val[:, -1, -1]
+    val_X, val_y_dec = val[:, :, :-1], val[:, -1, -1]
 
 
     # create a binary output vector (e.g.: Activity 4 => [0,0,0,0,1,0,0,...,0]
-    val_y_bin = np.zeros(n_classes * val_y.shape[0], dtype=np.int).reshape(val_y.shape[0], n_classes)
-    val_y = val_y.astype(int)
+    val_y_bin = np.zeros(n_classes * val_y_dec.shape[0], dtype=np.int).reshape(val_y_dec.shape[0], n_classes)
+    val_y_dec = val_y_dec.astype(int)
 
-    for counter, dec_val in enumerate(val_y):
+    for counter, dec_val in enumerate(val_y_dec):
         val_y_bin[counter, dec_val] = 1
 
 
     #print(val_X.shape, val_y.shape, val_y_bin.shape)
 
-    return val_X, val_y_bin
+    return val_X, val_y_bin, val_y_dec
 
 
 n_classes = 34
@@ -79,7 +101,7 @@ dataset = read_csv('prepared_subject5_ideal.csv', header=0, index_col=0)
 
 
 # split the dataset into input and output data and reshape input for LSTM [samples, timesteps, features]
-values_X, values_y = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes)
+values_X, values_y, _ = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes)
 
 # split into train and test sets
 train_to_test_ratio = 0.7
@@ -91,23 +113,27 @@ print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
 # network architecture
 # TODO: use a sequence as return (maybe more target y are needed)
+
+
+
 model = Sequential()
 model.add(Bidirectional(LSTM(100), input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(train_y.shape[1], activation='softmax'))
-model.load_weights("model.h5")
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc', cus_met.fbeta_score, ])
+model = load_model('my_model.h5')
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc']) #cus_met.fbeta_score,
 
 # start Training
-history = model.fit(train_X, train_y, batch_size=30, epochs=2, validation_data=[test_X, test_y],verbose=2)
+history = model.fit(train_X, train_y, batch_size=600, epochs=5, validation_data=[test_X, test_y],verbose=2)
 
 # save model to disk
-model.save_weights("model.h5")
+model.save('my_model.h5')
 
 # plot the history
-pyplot.plot(history.history['fbeta_score'], label='train')
-pyplot.plot(history.history['val_fbeta_score'], label='test')
-pyplot.legend()
+#pyplot.plot(history.history['fbeta_score'], label='train')
+#pyplot.plot(history.history['val_fbeta_score'], label='test')
+#pyplot.legend()
 #pyplot.show()
 
 
@@ -115,9 +141,9 @@ validate_dataset('subject5_ideal', model, n_timesteps, n_classes)
 
 validate_dataset('subject5_self', model, n_timesteps, n_classes)
 
-validate_dataset('subject3_ideal', model, n_timesteps, n_classes)
+#validate_dataset('subject3_ideal', model, n_timesteps, n_classes)
 
-validate_dataset('subject3_self', model, n_timesteps, n_classes)
+#validate_dataset('subject3_self', model, n_timesteps, n_classes)
 
 
 print('Ende')
