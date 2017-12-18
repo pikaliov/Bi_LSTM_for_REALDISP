9342e117acc5aac0fbc5514a5fa241bbc501e25a
Fese <fabian.fesenmayr@gmail.com>
2017-12-18 16:36:33 +0100

    added function to reduce the overrepresented class

--- train_Bi_LSTM.py
+++ train_Bi_LSTM.py
@@ -49,14 +49,15 @@ def validate_dataset(name, model, n_timesteps, n_classes):
 
 
 
-def split_dataset_into_input_and_output(dataset, timesteps, n_classes ):
+def split_dataset_into_input_and_output(dataset, timesteps, n_classes, reduce_overrepresented_class=False):
 
     # drop the timestamp
     dataset = dataset.drop('Sec', 0)
 
     # show the distribution of the activities in the dataset
-    # value_count = dataset['Activity'].value_counts(normalize=True)
-    # print(value_count)
+    if reduce_overrepresented_class:
+        value_count = dataset['Activity'].value_counts(normalize=True)
+        print('%.2f%% of the dataset belong to target class [%d] ' % ((value_count[0]*100), (value_count.index[0])))
 
     # dataset.dropna(inplace=True)
     val = dataset.values
@@ -75,11 +76,47 @@ def split_dataset_into_input_and_output(dataset, timesteps, n_classes ):
     if val_remainder > 0:
         val = val[:-val_remainder, :]
 
+
+
     # reshape data for LSTM [samples, timesteps, features] (round because: float => int)
     val = val.reshape((round(val.shape[0] / timesteps), timesteps, val.shape[1]))
 
     # shuffle the time sequences (shuffles the array along the first axis of a multi-dimensional array)
     np.random.shuffle(val)
+
+    # shrink the first overrepresented class to the double size of the second overrepresented class
+    # Attention: This shrink method should only be used, if the likelihood is high that the all following
+    # timesteps (in val.shape[1]) have the same class
+    if reduce_overrepresented_class:
+        deletion_count = int(round((val.shape[0] * (value_count[value_count.index[0]] - value_count[value_count.index[1]]))/2))
+
+        if deletion_count > 0:
+            val_shrinked = np.empty(
+                round(val.shape[0] * val.shape[1] * val.shape[2] - deletion_count * val.shape[1] * val.shape[2]))
+            val_shrinked = val_shrinked.reshape(round(val.shape[0] - deletion_count), timesteps, val.shape[2])
+            deleted_timesteps = 0
+            val_shrinked_index = 0
+            iterator = np.nditer(val[:, -1, -1], flags=['multi_index'])
+            while not iterator.finished:
+                mi = iterator.multi_index
+                if (iterator[0] == value_count.index[0] and deleted_timesteps < deletion_count):
+                    deleted_timesteps += 1
+                else:
+                    val_shrinked[val_shrinked_index, :, :] = val[mi[0], :, :]
+                    val_shrinked_index += 1
+
+                iterator.iternext()
+
+            # control statement
+            if deleted_timesteps == deletion_count and val_shrinked_index == val_shrinked.shape[0]:
+                val = val_shrinked
+                np.random.shuffle(val)
+                print("Reduced overrepresented class [%d] successfully!" % value_count.index[0])
+
+
+
+
+
 
     # split into input and output data
     # keep only the target y from the end of the sequence
@@ -101,7 +138,7 @@ def split_dataset_into_input_and_output(dataset, timesteps, n_classes ):
 
 
 n_classes = 34
-n_timesteps = 10
+n_timesteps = 20
 load_existing_model = False
 save_model_to_disk = True
 
@@ -110,7 +147,7 @@ dataset = read_csv('prepared_combination_subject5_ideal_et_al.csv', header=0, in
 
 
 # split the dataset into input and output data and reshape input for LSTM [samples, timesteps, features]
-values_X, values_y, _ = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes)
+values_X, values_y, _ = split_dataset_into_input_and_output(dataset, n_timesteps, n_classes, reduce_overrepresented_class=False)
 
 # split into train and test sets
 train_to_test_ratio = 0.7
@@ -119,9 +156,6 @@ train_X, test_X = values_X[:spl, :, :], values_X[spl:, :, :]
 train_y, test_y = values_y[:spl, :],    values_y[spl:, :]
 
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
-
-
-
 
 
 # load a existing model or create a new model
@@ -138,7 +172,7 @@ else:
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc']) #cus_met.fbeta_score,
 
 # start Training
-history = model.fit(train_X, train_y, batch_size=30, epochs=10, validation_data=[test_X, test_y],verbose=2)
+history = model.fit(train_X, train_y, batch_size=60, epochs=10, validation_data=[test_X, test_y],verbose=2)
 
 # save model to disk
 if save_model_to_disk:
